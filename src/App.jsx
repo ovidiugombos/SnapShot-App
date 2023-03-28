34c3f8159e57519f2fde1cbbc5c23485030067c1
ovidiugombos <gomboso@mail.com>
2023-03-28 10:26:01 +0300

    git init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Forms from "../../components/Forms";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="app--title">SnapShot</h1>
+      <Forms />
+    </div>
+  );
+}
